71e4a915a0212cfa579cba772f65255a0235861e
Rezvan Ehsani <43534885+RezvanEhsani@users.noreply.github.com>
2023-03-21 11:41:47 +0100

    Create mySessionConfig.sh

---
+++ Codes/mySessionConfig.sh
@@ -0,0 +1,50 @@
+R version 4.2.2 (2022-10-31 ucrt)
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+Running under: Windows 10 x64 (build 19044)
+
+Matrix products: default
+
+locale:
+[1] C
+
+attached base packages:
+[1] parallel  stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+ [1] FlowSOM_2.6.0         Biobase_2.58.0        BiocGenerics_0.44.0   flowCore_2.10.0       survminer_0.4.9      
+ [6] survival_3.4-0        ggfortify_0.4.15      Rtsne_0.16            ggrepel_0.9.2         ggpubr_0.5.0         
+[11] ggalluvial_0.12.3     pheatmap_1.0.12       NLP_0.2-1             Spectre_0.4.1         pbmcapply_1.5.1      
+[16] forcats_0.5.2         stringr_1.5.0         purrr_1.0.1           readr_2.1.3           tidyr_1.2.1          
+[21] tibble_3.1.8          tidyverse_1.3.2       progressr_0.13.0      caramellar_0.0.1.0000 gridExtra_2.3        
+[26] qwraps2_0.5.2         reshape2_1.4.4        FNN_1.1.3.1           ggplot2_3.4.0         igraph_1.3.5         
+[31] deldir_1.0-6          lubridate_1.9.0       timechange_0.2.0      truncnorm_1.0-8       spam_2.9-1           
+[36] dplyr_1.0.10          Rfast_2.0.6           RcppZiggurat_0.1.6    Rcpp_1.0.9            data.table_1.14.6    
+
+loaded via a namespace (and not attached):
+ [1] ggnewscale_0.4.8            googledrive_2.0.0           colorspace_2.0-3           
+ [4] ggsignif_0.6.4              colorRamps_2.3.1            ellipsis_0.3.2             
+ [7] cytolib_2.10.1              fs_1.5.2                    rstudioapi_0.14            
+[10] farver_2.1.1                fansi_1.0.3                 xml2_1.3.3                 
+[13] splines_4.2.2               knitr_1.41                  polyclip_1.10-4            
+[16] jsonlite_1.8.4              broom_1.0.2                 km.ci_0.5-6                
+[19] cluster_2.1.4               dbplyr_2.3.0                ggforce_0.4.1              
+[22] compiler_4.2.2              httr_1.4.4                  backports_1.4.1            
+[25] assertthat_0.2.1            Matrix_1.5-1                gargle_1.2.1               
+[28] cli_3.6.0                   tweenr_2.0.2                tools_4.2.2                
+[31] dotCall64_1.0-2             gtable_0.3.1                glue_1.6.2                 
+[34] carData_3.0-5               cellranger_1.1.0            vctrs_0.5.1                
+[37] xfun_0.36                   rvest_1.0.3                 lifecycle_1.0.3            
+[40] XML_3.99-0.13               rstatix_0.7.1               googlesheets4_1.0.1        
+[43] MASS_7.3-58.1               zoo_1.8-11                  scales_1.2.1               
+[46] RProtoBufLib_2.10.0         hms_1.1.2                   RColorBrewer_1.1-3         
+[49] KMsurv_0.1-5                stringi_1.7.12              S4Vectors_0.36.1           
+[52] rlang_1.0.6                 pkgconfig_2.0.3             matrixStats_0.63.0         
+[55] lattice_0.20-45             labeling_0.4.2              cowplot_1.1.1              
+[58] tidyselect_1.2.0            plyr_1.8.8                  magrittr_2.0.3             
+[61] R6_2.5.1                    generics_0.1.3              DBI_1.1.3                  
+[64] pillar_1.8.1                haven_2.5.1                 withr_2.5.0                
+[67] abind_1.4-5                 modelr_0.1.10               crayon_1.5.2               
+[70] car_3.1-1                   survMisc_0.5.6              utf8_1.2.2                 
+[73] tzdb_0.3.0                  grid_4.2.2                  readxl_1.4.1               
+[76] ConsensusClusterPlus_1.62.0 reprex_2.0.2                digest_0.6.31              
+[79] xtable_1.8-4                stats4_4.2.2                munsell_0.5.0 
